e708a1767cd2a275fb9254ff1b6403a010748899
milad82mi-byte <milad82mi@gmail.com>
2026-05-20 02:55:57 +0330

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e
 
-# گرفتن آخرین نسخه Xray به جای نسخه قدیمی
 RELEASE_URL="https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip"
 TMPDIR="$(mktemp -d)"
 
@@ -10,11 +9,11 @@ curl -sL "$RELEASE_URL" -o "$TMPDIR/xray.zip"
 unzip -q "$TMPDIR/xray.zip" -d "$TMPDIR"
 install -m 755 "$TMPDIR/xray" /usr/local/bin/xray
 
-echo "[g2ray] Downloading latest GeoIP and GeoSite..."
+echo "[g2ray] Downloading GeoIP and GeoSite..."
+mkdir -p /usr/local/share/xray
 curl -sL "https://github.com/v2fly/geoip/releases/latest/download/geoip.dat" -o /usr/local/share/xray/geoip.dat
 curl -sL "https://github.com/v2fly/domain-list-community/releases/latest/download/dlc.dat" -o /usr/local/share/xray/geosite.dat
 
-# ایجاد پوشه لاگ
 mkdir -p /var/log/xray
 
 rm -rf "$TMPDIR"
